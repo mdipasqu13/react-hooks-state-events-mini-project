50c362d5cdde8fc0ed78633f0fe50edbbfe5e8dc
Michael DiPasquale <michaeldipasquale313@gmail.com>
2024-03-17 20:48:43 -0400

    completed lab

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,11 +1,12 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
-  return (
-    <div className="tasks">
-      {/* display a list of tasks using Task component */}
-    </div>
-  );
+function TaskList({ tasks, onDeleteTask }) {
+  const taskElements = tasks.map(task => (
+    <Task key={task.id} text={task.text} category={task.category} onDeleteTask={onDeleteTask} />
+  ));
+
+  return <div className="tasks">{taskElements}</div>;
 }
 
 export default TaskList;
